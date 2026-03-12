76fe007cafa3a5ed0410698630a5c953e70ffa8b
Truongpio16 <hdtruong160705@gmail.com>
2026-03-12 22:01:05 +0700

    Add files via upload

---
+++ HotelManager1/HotelManager1/Model/Lop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManager1.Model
+{
+    public class Lop
+    {
+        public string TenLop { get; set; }
+        public string MaLop { get; set; }
+    }
+}
---
+++ HotelManager1/HotelManager1/Model/TaiKhoan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManager1.Model
+{
+    public class TaiKhoan
+    {
+        public string TenDN { get; set; }
+        public string MatKhau { get; set; }
+ 
+    }
+}
